3e3fb81f30ee4edf7662e3512220b98999dac542
yutytuty <49990984+yutytuty@users.noreply.github.com>
2022-12-12 20:58:43 +0200

    added basic entity interface

---
+++ src/collosalcyberadventure/client/entity.py
@@ -0,0 +1,85 @@
+import arcade
+
+
+class IEntity:
+    def draw(self, window: arcade.Window):
+        """ Draws the entity on the screen
+
+        Parameters
+        ----------
+        window: arcade.Window
+            The window you want to draw the entity on
+
+        Raises
+        ------
+        NotImplementedError
+            If the method is not overrided
+
+        Returns
+        -------
+        None
+        """
+        raise NotImplementedError("draw() method not implemented")
+
+    def move(self, delta_time: float):
+        """
+
+        Parameters
+        ----------
+        delta_time: float
+            time between frame refresh
+
+        Raises
+        ------
+        NotImplementedError
+            If the method is not overrided
+
+        Returns
+        -------
+        None
+        """
+        raise NotImplementedError("move() method not implemented")
+
+    def update(self):
+        """
+
+        Raises
+        ------
+        NotImplementedError
+            If the method is not overrided
+
+        Returns
+        -------
+        None
+        """
+        raise NotImplementedError("update() method not implemented")
+
+    def getX(self) -> int:
+        """
+
+        Raises
+        ------
+        NotImplementedError
+            If the method is not overrided
+
+        Returns
+        -------
+        int
+            player x
+        """
+        raise NotImplementedError("getX() method not implemented")
+
+    def getY(self):
+        """
+
+        Raises
+        ------
+        NotImplementedError
+            If the method is not overrided
+
+        Returns
+        -------
+        int
+            player y
+        """
+        raise NotImplementedError("getY() method not implemented")
