c64a70a2857ed4b29e9e8a58dd2b78dc622acc65
yutytuty <49990984+yutytuty@users.noreply.github.com>
2022-12-13 07:58:04 +0200

    added keyboard methods

--- src/collosalcyberadventure/client/window.py
+++ src/collosalcyberadventure/client/window.py
@@ -39,7 +39,8 @@ class IWindow(arcade.Window):
         """
         raise NotImplementedError("get_keyboard_handler() method not implemented")
 
-class Window(arcade.Window):
+
+class Window(arcade.Window, IWindow):
     """ Main window class
 
     Attributes
@@ -48,24 +49,29 @@ class Window(arcade.Window):
     height: int > 0
     title : str
     background_color : tuple[int, int, int] | tuple[int, int, int, int]
+    keyboard_handler : IKeyboardHandler
+        saves the state of the keyboard and handles keyboard logic
     """
+
     def __init__(self,
                  width: int,
                  height: int,
                  title: str,
-                 background_color: tuple[int, int, int] | tuple[int, int, int, int]):
+                 background_color: tuple[int, int, int] | tuple[int, int, int, int],
+                 keyboard_handler: IKeyboardHandler):
         super().__init__(width, height, title)
         self.width: Final = width
         self.height: Final = height
         self.title: Final = title
         self.background_color = background_color
+        self.keyboard_handler = keyboard_handler
 
         arcade.set_background_color(background_color)
 
     def setup(self):
         """Set up window
 
-        Sets up window. Call this once to restart game.
+        Sets up window. Call this again to restart game.
 
         Returns
         -------
@@ -83,3 +89,13 @@ class Window(arcade.Window):
         self.clear()
         # code to draw screen
 
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.W or symbol == arcade.key.A or symbol == arcade.key.S or symbol == arcade.key.D:
+            self.keyboard_handler.set_key(symbol, True)
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.W or symbol == arcade.key.A or symbol == arcade.key.S or symbol == arcade.key.D:
+            self.keyboard_handler.set_key(symbol, False)
+
+    def get_keyboard_handler(self) -> IKeyboardHandler:
+        return self.keyboard_handler
