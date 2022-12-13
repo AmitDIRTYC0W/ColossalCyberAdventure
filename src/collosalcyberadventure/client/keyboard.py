84c566b71620a5cf638cb348814ad2534fa851d6
yutytuty <49990984+yutytuty@users.noreply.github.com>
2022-12-13 07:46:51 +0200

    added set_key method

--- src/collosalcyberadventure/client/keyboard.py
+++ src/collosalcyberadventure/client/keyboard.py
@@ -5,3 +5,6 @@ class IKeyboardHandler:
     """This class holds all the key logic"""
     def get_pressed_keys(self) -> dict[int, bool]:
         raise NotImplementedError("get_pressed_keys() method not implemented")
+
+    def set_key(self, key: int, value: bool):
+        raise NotImplementedError("set_key() method not implemented")
