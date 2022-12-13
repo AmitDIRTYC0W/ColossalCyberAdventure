9efd1a5104d08848310a96754bc127eead18d473
roei <roeiyishayry@gmail.com>
2022-12-13 11:24:32 +0200

    Fix movement - Kanye now moves stay tuned

--- src/collosalcyberadventure/client/player.py
+++ src/collosalcyberadventure/client/player.py
@@ -5,10 +5,6 @@ import arcade
 class Player(arcade.Sprite, IEntity):
     def __init__(self, resource_name, speed):
         super().__init__(resource_name)
-        # self.a_pressed = False
-        # self.d_pressed = False
-        # self.w_pressed = False
-        # self.s_pressed = False
         self.speed = speed
 
     def update(self):
@@ -20,6 +16,9 @@ class Player(arcade.Sprite, IEntity):
         """
         self.center_x += self.change_x
         self.center_y += self.change_y
+        print(self.change_x, self.center_x)
+
+
 
     def getX(self):
         """ Returns player's X coordinate
@@ -39,7 +38,7 @@ class Player(arcade.Sprite, IEntity):
         """
         return self.center_y
 
-    def update_player_speed(self, keyboard_state: dict[str, bool]):
+    def update_player_speed(self, keyboard_state: dict[int, bool]):
         """ Updates player speed - what direction he should move to according to what key was pressed
 
         Returns
@@ -49,13 +48,13 @@ class Player(arcade.Sprite, IEntity):
         self.change_x = 0
         self.change_y = 0
 
-        if keyboard_state["w"] and not keyboard_state["s"]:
+        if keyboard_state[arcade.key.W] and not keyboard_state[arcade.key.S]:
             self.change_y = self.speed
-        elif keyboard_state["s"] and not keyboard_state["w"]:
+        elif keyboard_state[arcade.key.S] and not keyboard_state[arcade.key.W]:
             self.change_y = -self.speed
-        if keyboard_state["a"] and not keyboard_state["d"]:
+        if keyboard_state[arcade.key.A] and not keyboard_state[arcade.key.D]:
             self.change_x = -self.speed
-        elif keyboard_state["d"] and not keyboard_state["a"]:
+        elif keyboard_state[arcade.key.D] and not keyboard_state[arcade.key.A]:
             self.change_x = self.speed
 
 
