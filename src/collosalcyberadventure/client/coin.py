d7cf52e4ac8dca6a6c0403d8bf9dcd43419a58cd
roei <roeiyishayry@gmail.com>
2022-12-14 15:25:44 +0200

    Fix documentation

---
+++ src/collosalcyberadventure/client/coin.py
@@ -0,0 +1,19 @@
+from collosalcyberadventure.client.entity import IEntity
+import arcade
+
+
+class Coin(arcade.Sprite, IEntity):
+    def __init__(self, resource_name, pos_x, pos_y):
+        super().__init__(resource_name)
+        self.center_x = pos_x
+        self.center_y = pos_y
+
+    def get_position(self) -> tuple:
+        """returns the coin relative position to the map
+
+        :return:
+        tuple(int, int) : tuple that contains player x, y position
+        """
+
+        return tuple((self.center_x, self.center_y))
+
