b3306bee680453bc9bea309e4976eec3656ebf88
yutytuty <49990984+yutytuty@users.noreply.github.com>
2022-12-16 18:45:34 +0200

    added basic keyboard handler

--- src/collosalcyberadventure/client/keyboard.py
+++ src/collosalcyberadventure/client/keyboard.py
@@ -1,4 +1,4 @@
-import arcade.key
+from arcade import key as k
 
 
 class IKeyboardHandler:
@@ -8,3 +8,15 @@ class IKeyboardHandler:
 
     def set_key(self, key: int, value: bool):
         raise NotImplementedError("set_key() method not implemented")
+
+
+class KeyboardHandler(IKeyboardHandler):
+    def __init__(self):
+        self.pressed_keys = {k.W: False, k.A: False, k.S: False, k.D: False}
+
+    def get_pressed_keys(self) -> dict[int, bool]:
+        return self.pressed_keys
+
+    def set_key(self, key: int, value: bool):
+        if key in self.pressed_keys.keys():
+            self.pressed_keys[key] = value
