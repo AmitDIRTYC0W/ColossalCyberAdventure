14de02e177e20de1ae1b3d4ccff85c1c8ce1935c
yutytuty <49990984+yutytuty@users.noreply.github.com>
2022-12-16 18:45:24 +0200

    wrote test for initializing window

---
+++ tests/test_collosalcyberadventure.py
@@ -0,0 +1,26 @@
+import arcade
+import asyncio
+
+from interruptingcow import timeout
+
+from collosalcyberadventure.client.keyboard import KeyboardHandler
+from collosalcyberadventure.client.window import Window
+
+
+def test_window_initialization():
+    """Opens the window for 3 seconds
+
+    Raises
+    ------
+    AssertionError:
+        If the program exits before 3 seconds have passed
+
+    """
+    try:
+        with timeout(3, asyncio.CancelledError):
+            window = Window(800, 600, "test window", arcade.color.TAN, KeyboardHandler())
+            window.setup()
+            window.run()
+            assert False
+    except asyncio.CancelledError:
+        assert True
