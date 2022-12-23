5af286c107d7140f7a0334d09184abecc5fc59cc
immediate-signature <94056369+immediate-signature@users.noreply.github.com>
2022-12-23 16:43:34 +0200

    tilemap
    The tilemap with portals plus all of the tilesets and the pictures they are based on

---
+++ [A]WaterFall_pipo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="[A]WaterFall_pipo" tilewidth="32" tileheight="32" tilecount="576" columns="32">
+ <image source="[A]WaterFall_pipo.png" width="1024" height="576"/>
+</tileset>
