185cdea4d30b321232a65d635a92355caf2b228d
immediate-signature <94056369+immediate-signature@users.noreply.github.com>
2022-12-23 16:57:35 +0200

    Delete input_prompts_kenney.tsx

--- input_prompts_kenney.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="input_prompts_kenney" tilewidth="32" tileheight="32" tilecount="216" columns="18">
- <image source="input_prompts_kenney.png" width="578" height="408"/>
-</tileset>
