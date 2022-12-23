40796fd625538dc2a9e7dd41a60794d108305ca5
immediate-signature <94056369+immediate-signature@users.noreply.github.com>
2022-12-23 16:51:30 +0200

    Add files via upload

---
+++ resources/map/tileset/dffd.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="dffd" tilewidth="2048" tileheight="4256" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="1344" source="[A]Dirt_pipo.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="384" source="[A]Flower_pipo.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="2112" source="[A]Grass_pipo.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="384" source="[A]Wall-Up_pipo.png"/>
+ </tile>
+ <tile id="4">
+  <image width="2048" height="1536" source="[A]Water_pipo.png"/>
+ </tile>
+ <tile id="5">
+  <image width="1024" height="576" source="[A]WaterFall_pipo.png"/>
+ </tile>
+ <tile id="6">
+  <image width="256" height="4256" source="[Base]BaseChip_pipo.png"/>
+ </tile>
+ <tile id="7">
+  <image width="578" height="408" source="input_prompts_kenney.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="192" source="LightShadow_pipo.png"/>
+ </tile>
+</tileset>
