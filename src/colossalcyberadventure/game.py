9060b95c72d4808cdb65db035da877e947500f9b
yutytuty <49990984+yutytuty@users.noreply.github.com>
2023-01-05 16:50:12 +0200

    Add tilemaps

--- src/colossalcyberadventure/game.py
+++ src/colossalcyberadventure/game.py
@@ -1,33 +1,17 @@
-from typing import Final
 import arcade
 
 from arcade import key as k
 
 from colossalcyberadventure.player import Player
+from constants import *
 
 
 class ColossalCyberAdventure(arcade.Window):
-    """ Main window class
-
-    Attributes
-    ----------
-    width : int
-        pixels, greater than 0
-    height:
-        pixels, greater than 0
-    title : str
-    """
-
+    TITLE = "Colossal Cyber Adventure"
     BACKGROUND_COLOR = arcade.color.AMAZON
 
-    def __init__(self,
-                 width: int,
-                 height: int,
-                 title: str,):
-        super().__init__(width, height, title)
-        self.width: Final[int] = width
-        self.height: Final[int] = height
-        self.title: Final[str] = title
+    def __init__(self):
+        super().__init__(title=ColossalCyberAdventure.TITLE, fullscreen=True)
 
         arcade.set_background_color(ColossalCyberAdventure.BACKGROUND_COLOR)
 
@@ -42,13 +26,18 @@ class ColossalCyberAdventure(arcade.Window):
 class GameView(arcade.View):
     """View of the actual game
     """
+
     BACKGROUND_COLOR = arcade.color.JET
+    MAP_PATH = "resources/map.tmx"
 
     def __init__(self):
         super().__init__()
 
         self.player = Player()
-        self.pressed_keys = {k.W: False, k.A: False, k.S: False, k.D: False}
+        self.keyboard_state = {k.W: False, k.A: False, k.S: False, k.D: False}
+        self.camera = arcade.camera.Camera(self.window.width, self.window.height)
+        self.tile_map = arcade.load_tilemap(GameView.MAP_PATH, TILE_SCALING)
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
 
         arcade.set_background_color(GameView.BACKGROUND_COLOR)
 
@@ -58,13 +47,14 @@ class GameView(arcade.View):
         self.player.draw()
 
     def on_update(self, delta_time: float):
-        self.player.update_player_speed(self.pressed_keys)
+        self.player.update_player_speed(self.keyboard_state)
+        self.player.update_animation()
         self.player.update()
 
     def on_key_press(self, symbol: int, modifiers: int):
-        if symbol in self.pressed_keys.keys():
-            self.pressed_keys[symbol] = True
+        if symbol in self.keyboard_state.keys():
+            self.keyboard_state[symbol] = True
 
     def on_key_release(self, symbol: int, _modifiers: int):
-        if symbol in self.pressed_keys.keys():
-            self.pressed_keys[symbol] = False
+        if symbol in self.keyboard_state.keys():
+            self.keyboard_state[symbol] = False
