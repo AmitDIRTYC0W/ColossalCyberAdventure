adf59a1980ede48e8d3c6a68913edf1f80f96762
yutytuty <49990984+yutytuty@users.noreply.github.com>
2023-01-25 17:05:48 +0200

    Add ip input field

--- src/colossalcyberadventure/start_screen.py
+++ src/colossalcyberadventure/start_screen.py
@@ -8,6 +8,7 @@ class StartScreenView(arcade.View):
     """shows the view of the starting screen and lets you press the buttons"""
 
     BUTTON_WIDTH = 200
+    IP_LABEL_WIDTH = 30
     BUTTON_BOTTOM = 20
 
     def __init__(self):
@@ -27,6 +28,14 @@ class StartScreenView(arcade.View):
         # Create the buttons
         start_button = arcade.gui.UIFlatButton(text="Start Game", width=StartScreenView.BUTTON_WIDTH)
         self.v_box.add(start_button.with_space_around(bottom=StartScreenView.BUTTON_BOTTOM))
+
+        # Create ip field
+        ip_h_box = arcade.gui.UIBoxLayout(vertical=False)
+        ip_label = arcade.gui.UILabel(text="IP: ", width=StartScreenView.IP_LABEL_WIDTH)
+        ip_field = arcade.gui.UIBorder(child=arcade.gui.UIInputText(width=StartScreenView.BUTTON_WIDTH, height=20))
+        ip_h_box.add(ip_label)
+        ip_h_box.add(ip_field)
+        self.v_box.add(ip_h_box.with_space_around(bottom=StartScreenView.BUTTON_BOTTOM))
 
         settings_button = arcade.gui.UIFlatButton(text="Settings", width=StartScreenView.BUTTON_WIDTH)
         self.v_box.add(settings_button.with_space_around(bottom=StartScreenView.BUTTON_BOTTOM))
