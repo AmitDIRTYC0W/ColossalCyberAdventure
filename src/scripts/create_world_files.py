58b5a1789bad6ed7906f16633389bc05c49c3e79
yutytuty <49990984+yutytuty@users.noreply.github.com>
2023-02-22 17:25:46 +0200

    Move files and add base map and world

--- src/scripts/create_world_files.py
+++ src/scripts/create_world_files.py
@@ -3,7 +3,7 @@ import json
 MAP_WIDTH_PX = 2400
 MAP_HEIGHT_PX = 1350
 
-with open("resources/map/example.tmx", "r") as ex:
+with open("../colossalcyberadventure/resources/map/example.tmx", "r") as ex:
     text = ex.read()
 
 
@@ -18,6 +18,6 @@ for y in range(30):
         j["maps"].append({"fileName": f"{x}-{y}.tmx", "height": 1440, "width": 2560, "x": y * 2560, "y": -x * 1440})
         create_file(f"{x}-{y}")
 
-world = open("resources/map/map.world", "w")
+world = open("../colossalcyberadventure/resources/map/map.world", "w")
 json.dump(j, world, indent=4)
 world.close()
