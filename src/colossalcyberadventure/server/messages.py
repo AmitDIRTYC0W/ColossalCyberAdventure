3ea2a730e751beb46e8ee63aa02331c4ae217c89
SG_Robin <shlomogdd@gmail.com>
2023-02-27 12:39:27 +0200

    changed it to identification

--- src/colossalcyberadventure/server/messages.py
+++ src/colossalcyberadventure/server/messages.py
@@ -1,12 +1,13 @@
 import capnp
 
 capnp.remove_import_hook()
-log_in_capnp = capnp.load("../../../submodules/ColossalCyberAdventureMessages/log_in.capnp")
+identification_capnp = capnp.load("../../../submodules/ColossalCyberAdventureMessages/identification.capnp")
 
 
-def create_login_request(username, password):
-    return log_in_capnp.LogInRequest.new_message(username=username, password=password)
+def create_identification_request(username, password, register):
+    return identification_capnp.IdentificationRequest.new_message(
+        username=username, password=password, register=register)
 
 
-def read_login_response(b: bytes):
-    return log_in_capnp.LogInResponse.from_bytes_packed(b)
+def read_identification_response(b: bytes):
+    return identification_capnp.IdentificationRequest.from_bytes_packed(b)
--- src/colossalcyberadventure/server/protocol.py
+++ src/colossalcyberadventure/server/protocol.py
@@ -4,10 +4,10 @@ from aioquic.asyncio import QuicConnectionProtocol
 from aioquic.quic import events
 from aioquic.quic.events import StreamDataReceived
 
-from colossalcyberadventure.server.messages import create_login_request, read_login_response
+from colossalcyberadventure.server.messages import create_identification_request, read_identification_response
 
 
-class LogInProtocol(QuicConnectionProtocol):
+class IdentificationProtocol(QuicConnectionProtocol):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._ack_waiter = None
@@ -16,11 +16,11 @@ class LogInProtocol(QuicConnectionProtocol):
     def quic_event_received(self, event: events.QuicEvent) -> None:
         if self._ack_waiter is not None:
             if isinstance(event, StreamDataReceived):
-                self.response = read_login_response(event.data)
+                self.response = read_identification_response(event.data)
 
-    async def send_log_in(self, username, password):
+    async def send_identification(self, username, password, register):
         stream_id = self._quic.get_next_available_stream_id()
-        data = create_login_request(username, password).to_bytes_packed()
+        data = create_identification_request(username, password, register).to_bytes_packed()
         self._quic.send_stream_data(stream_id, data, end_stream=True)
         waiter = self._loop.create_future()
         self._ack_waiter = waiter
