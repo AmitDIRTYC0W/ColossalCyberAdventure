ad884e5c05dfae66c89950aae1cc13c369e7fc26
yutytuty <49990984+yutytuty@users.noreply.github.com>
2023-03-06 17:32:36 +0200

    Add script to dump terain hitboxes to file

---
+++ src/scripts/dump_terrain_hitbox.py
@@ -0,0 +1,21 @@
+import json
+
+import arcade
+
+scene = arcade.Scene.from_tilemap(arcade.load_tilemap("../colossalcyberadventure/resources/map/0-0.tmx", layer_options={
+    "Trees": {
+        "use_spatial_hash": True,
+    }
+}))
+
+# with open("static_hitboxes.txt", "w") as f:
+# for sprite in scene.name_mapping["Trees"]:
+# f.write(str(sprite.get_adjusted_hit_box()) + "\n")
+
+hitboxes = {"Trees": []}
+
+for sprite in scene.name_mapping["Trees"]:
+    hitboxes["Trees"].append(str(sprite.get_adjusted_hit_box()))
+
+with open("static_hitboxes.txt", "w") as f:
+    json.dump(hitboxes, f, indent=4)
