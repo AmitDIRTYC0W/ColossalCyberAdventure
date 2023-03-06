6cd4a97e70e234bdb5deb8074737ebbf50056838
yutytuty <49990984+yutytuty@users.noreply.github.com>
2023-03-06 19:17:32 +0200

    Change something (?)

--- src/scripts/dump_terrain_hitbox.py
+++ src/scripts/dump_terrain_hitbox.py
@@ -10,8 +10,10 @@ scene = arcade.Scene.from_tilemap(arcade.load_tilemap("../colossalcyberadventure
 
 hitboxes = {"Trees": []}
 
-for sprite in scene.name_mapping["Trees"]:
-    hitboxes["Trees"].append(str(sprite.get_adjusted_hit_box()))
+for i, sprite in enumerate(scene.name_mapping["Trees"]):
+    hitboxes["Trees"].append([])
+    for x, y in sprite.get_adjusted_hit_box():
+        hitboxes["Trees"][i].append((x, y))
 
 with open("static_hitboxes.json", "w") as f:
     json.dump(hitboxes, f, indent=4)
