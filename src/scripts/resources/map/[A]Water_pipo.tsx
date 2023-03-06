cdf5958483127b3c4f7614446c98d24221f606c8
yutytuty <49990984+yutytuty@users.noreply.github.com>
2023-03-07 00:02:14 +0200

    Add map files

---
+++ src/scripts/resources/map/[A]Water_pipo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="[A]Water_pipo" tilewidth="32" tileheight="32" tilecount="3072" columns="64">
+ <image source="./[A]Water_pipo.png" width="2048" height="1536"/>
+</tileset>
